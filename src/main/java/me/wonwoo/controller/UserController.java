128d87ae910c4e73ed50dabd94f92b4e85d15bc9
alforik <neo0603@naver.com>
2018-07-04 07:14:57 +0900

    usercontoller requriedargsconstructor autowired 로 변경해도 됨..

--- src/main/java/me/wonwoo/controller/UserController.java
+++ src/main/java/me/wonwoo/controller/UserController.java
@@ -1,23 +1,22 @@
 package me.wonwoo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.RequiredArgsConstructor;
 import me.wonwoo.domain.User;
-import me.wonwoo.repository.UserRepository;
 import me.wonwoo.service.UserService;
 
 /**
  * Created by wonwoo on 2016. 7. 16..
  */
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class UserController {
 
-	
-  private final UserService userService;
+	@Autowired
+  private UserService userService;
   //private final UserRepository userRepository;
 
   @GetMapping("/findOne/{id}")
